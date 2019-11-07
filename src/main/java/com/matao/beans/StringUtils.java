9a743859bcd8dc13d03426cf0500dba35e3a4aeb
matao-git <2237362443@qq.com>
2019-11-07 20:22:46 +0800

    haveValue+isPhoneNum+isEmail

--- src/main/java/com/matao/beans/StringUtils.java
+++ src/main/java/com/matao/beans/StringUtils.java
@@ -1,4 +1,8 @@
 package com.matao.beans;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 
     * @ClassName: StringUtils
@@ -19,5 +23,39 @@ public class StringUtils {
 	public static boolean isBlank(String str) {
 		
 		return (str == null||str.trim().equals(""));
+	}
+	/**
+	 * 
+	    * @Title: haveValue
+	    * @Description:判断字符创是否有空值，空引号也算没值
+	    * @param @param str
+	    * @param @return    参数
+	    * @return boolean    返回类型
+	    * @throws
+	 */
+	public static boolean haveValue(String str) {
+		//trim方法 自动去除前后的空值
+		return !(str == null||str.trim().equals(""));
+	}
+	/**
+	 * 
+	    * @Title: isPhoneNum
+	    * @Description: 验证手机号码
+	    * @param @param str
+	    * @param @return    参数
+	    * @return boolean    返回类型
+	    * @throws
+	 */
+	public static boolean isPhoneNum(String str) {
+		String regex="^((13[0-9])|(14[5|7])|(15[0-3][5-9])|(17[013678])|(18[0,5-9]))/d{8}$";
+		Pattern compile = Pattern.compile(regex);
+		Matcher m = compile.matcher(str);
+		return m.find();
+	}
+	public static boolean isEmail(String email) {
+		String regex= "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+		Pattern compile = Pattern.compile(regex);
+		Matcher m = compile.matcher(email);
+		return m.find();
 	}
 }
